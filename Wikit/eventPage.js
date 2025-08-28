8391ad8f1bc3c9d6543cc0361cc314ccf2741dc3
Murathan <mrthelcuman@gmail.com>
2025-08-28 11:03:53 +0300

    Popup extension issue is solved

--- Wikit/eventPage.js
+++ Wikit/eventPage.js
@@ -1,7 +1,6 @@
-// This function runs once, when the extension is first installed or updated.
 chrome.runtime.onInstalled.addListener(function() {
     chrome.contextMenus.create({
-        "id": "wikit",
+        "id": "Wikit",
         "title": "Wikit",
         "contexts": ["selection"]
     });
@@ -11,18 +10,20 @@ function fixedEncodeURI (str) {
     return encodeURI(str).replace(/%5B/g, '[').replace(/%5D/g, ']');
 }
 
-// This listener will be re-registered each time the service worker starts.
-chrome.contextMenus.onClicked.addListener(function(clickData){   
-    if (clickData.menuItemId == "wikit" && clickData.selectionText){    
-        var wikiUrl = "https://en.wikipedia.org/wiki/" + fixedEncodeURI(clickData.selectionText);
-        var createData = {
-            "url": wikiUrl,
-            "type": "popup",
-            "top": 5,
-            "left": 5,
-            "width": Math.round(screen.availWidth/2), // It's good practice to round pixel values
-            "height": Math.round(screen.availHeight/2)
-        };              
-        chrome.windows.create(createData, function(){});        
+chrome.contextMenus.onClicked.addListener(function(clickData){
+    if (clickData.menuItemId == "Wikit" && clickData.selectionText){
+        chrome.system.display.getInfo(function(displays){
+            var screenSize = displays[0];
+            var wikiUrl = "https://en.wikipedia.org/wiki/" + fixedEncodeURI(clickData.selectionText);
+            var createData = {
+                "url": wikiUrl,
+                "type": "popup",
+                "top": 5,
+                "left": 5,
+                "width": Math.floor(screenSize.bounds.width) / 2,
+                "height": Math.floor(screenSize.bounds.height) / 2  
+            };
+            chrome.windows.create(createData, function(){}); 
+        });                 
     }
 });
